7873538257100906db30eb5b564790bcf2e436d7
ricar <ricar@10.179.10.217>
2025-09-22 18:12:09 +0100

    class pintura

--- src/main/java/Laboratorio_3/Pintura.java
+++ src/main/java/Laboratorio_3/Pintura.java
@@ -1,6 +1,38 @@
 package Laboratorio_3;
 
-public class Pintura {
+
+
+public class Pintura extends Obra{
+	
+		String tecnica;
+
+		
+
+		
+
+		public Pintura(String titulo, String autor, int ano, String tecnica) {
+			super(titulo, autor, ano);
+			this.tecnica = tecnica;
+		}
+
+		public String getTecnica() {
+			return tecnica;
+		}
+
+		public void setTecnica(String tecnica) {
+			this.tecnica = tecnica;
+		}
+
+		
+		public String toString() {
+			return "pintura [tecnica=" + tecnica + "]";
+		}
+
+		
+			
+		
+		
+	
 
   
 }
