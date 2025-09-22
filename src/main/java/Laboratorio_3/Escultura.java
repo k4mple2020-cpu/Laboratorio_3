9aa5906780dc8b2e9d3a3e9fe1ca9f169d74fe05
Bruninha9 <brunajasmin15@gmail.com>
2025-09-22 17:12:53 +0100

    alteraçao classe Escultura e classe Obra

--- src/main/java/Laboratorio_3/Escultura.java
+++ src/main/java/Laboratorio_3/Escultura.java
@@ -1,6 +1,22 @@
 package Laboratorio_3;
 
-public class Escultura {
-
-  
+public class Escultura extends Obra {
+	private String material;
+	public Escultura (String titulo, String autor, int anoCriacao, String material) {
+		super (titulo, autor, anoCriacao);
+		this.material = material;
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial (String material) {
+		this.material = material;
+	}
+	@Override
+	public void imprimirDetalhes() {
+		super.imprimirDetalhes();
+System.out.println ("Material:" + this.material);
+	}
 }
+	
+	
--- src/main/java/Laboratorio_3/Obra.java
+++ src/main/java/Laboratorio_3/Obra.java
@@ -2,5 +2,15 @@ package Laboratorio_3;
 
 public class Obra {
 
+	public Obra(String titulo, String autor, int anoCriacao) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void imprimirDetalhes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
   
 }
