8e1593e1de78910fad1f2b80f014d4967475091e
ricar <ricar@10.179.10.217>
2025-09-22 18:11:55 +0100

    realizar a class obra e a class pintura

--- src/main/java/Laboratorio_3/Obra.java
+++ src/main/java/Laboratorio_3/Obra.java
@@ -1,6 +1,46 @@
 package Laboratorio_3;
 
 public class Obra {
+		
+		String titulo;
+		String autor;
+		int ano;
+	
+	
+		
+		public Obra(String titulo, String autor, int ano) {
+			super();
+			this.titulo = titulo;
+			this.autor = autor;
+			this.ano = ano;
+		}
+
+		public Obra(String titulo, String autor) {
+			this.titulo = titulo;
+			this.autor = autor;
+		
+		}
+		
+		public String getTitulo() {
+			return titulo;
+		}
+		public String getAutor() {
+			return autor;
+		}
+		public int getAno() {
+			return ano;
+		}
+
+		@Override
+		public String toString() {
+			return "Obra [titulo=" + titulo + ", autor=" + autor + ", ano=" + ano + "]";
+		}
+
+	
+		
+		
+		
+	}
 
   
-}
+
