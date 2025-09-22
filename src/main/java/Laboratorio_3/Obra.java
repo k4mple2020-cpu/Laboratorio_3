cc0c7c29a142f447ae3e64bff505851dd67fcf07
CarlosCarr8 <grimaldos882@gmail.com>
2025-09-22 13:11:39 -0500

    Obra class complementation.

--- src/main/java/Laboratorio_3/Obra.java
+++ src/main/java/Laboratorio_3/Obra.java
@@ -1,16 +1,22 @@
 package Laboratorio_3;
 
+
 public class Obra {
+    String titulo;      
+    String autor;       
+    int anoCriacao;     
 
-	public Obra(String titulo, String autor, int anoCriacao) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public void imprimirDetalhes() {
-		// TODO Auto-generated method stub
-		
-	}
+    
+    public Obra(String titulo, String autor, int anoCriacao) {
+        this.titulo = titulo;       
+        this.autor = autor;         
+        this.anoCriacao = anoCriacao; 
+    }
 
 
-  
+    public void imprimirDetalhes() {
+        System.out.println("Título: " + titulo);      
+        System.out.println("Autor: " + autor);       
+        System.out.println("Ano de criação: " + anoCriacao); 
+    }
 }
