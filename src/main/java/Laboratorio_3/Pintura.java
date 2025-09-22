480819002f89b22d81080537336471e7b2ec234e
CarlosCarr8 <grimaldos882@gmail.com>
2025-09-22 12:48:50 -0500

    Implement Pintura class with tecnica attribute

--- src/main/java/Laboratorio_3/Pintura.java
+++ src/main/java/Laboratorio_3/Pintura.java
@@ -1,6 +1,24 @@
 package Laboratorio_3;
 
-public class Pintura {
+public class Pintura extends Obra {
+    private String tecnica;
 
-  
+    public Pintura(String titulo, String autor, int anoCriacao, String tecnica) {
+        super(titulo, autor, anoCriacao);
+        this.tecnica = tecnica;
+    }
+
+    public String getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    @Override
+    public void imprimirDetalhes() {
+        super.imprimirDetalhes();
+        System.out.println("Técnica: " + this.tecnica);
+    }
 }
