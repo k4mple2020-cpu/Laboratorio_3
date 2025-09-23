2dc7cb0dbe3b61cf7ef674bb3a9c003badf25af7
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2025-09-23 18:46:17 +0100

    Added class Main

--- src/main/java/Laboratorio_3/Main.java
+++ src/main/java/Laboratorio_3/Main.java
@@ -1,5 +1,8 @@
 package Laboratorio_3;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * @author Diogo Ferreira 51084
  * @author Bruna da Costa 52488
@@ -13,7 +16,63 @@ public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println(TecnicaPintura.ACRILICO);
+    GestorObras gestor = new GestorObras();
+    Scanner input = new Scanner(System.in);
+
+    while (true) {
+
+      System.out.println("Welcome to the Main Menu! Please choose an option");
+      System.out.println("1 - Register new Art Piece");
+      System.out.println("2 - Display the details of all art pieces");
+      System.out.println("3 - Search a piece by title");
+      System.out.println("4 - Display the title and author of an oil painting of a chosen type of canvas");
+      System.out.println("5 - Display all the details of all art pieces grouped by type");
+      System.out.println("6 - Exit the Program");
+
+      try {
+
+        int userChoice = input.nextInt();
+        input.nextLine();
+
+        switch (userChoice) {
+          case 1:
+            gestor.createObra();
+            break;
+
+          case 2:
+            gestor.listObras();
+            break;
+
+          case 3:
+            gestor.getObraByTitulo();
+            break;
+
+          case 4:
+            gestor.listPinturaOleoByTela();
+            break;
+
+          case 5:
+            gestor.listObrasBySpec();
+            break;
+
+          case 6:
+            System.out.println("Exiting...");
+            System.exit(0);
+
+          default:
+            System.out.println("Please Choose a number between 1 and 6!!");
+            break;
+
+        }
+
+      } catch (InputMismatchException e) {
+
+        System.out.println("Please enter a valid number");
+        continue;
+
+      }
+
+    }
 
   }
 }
