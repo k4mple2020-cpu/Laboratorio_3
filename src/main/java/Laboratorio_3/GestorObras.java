81d8761778c07602ea6f3321b88bc2332ad5ce7c
Falconz0012 <tomasfalcao.26@gmail.com>
2025-09-23 13:32:49 +0100

    wip GestorObras

--- src/main/java/Laboratorio_3/GestorObras.java
+++ src/main/java/Laboratorio_3/GestorObras.java
@@ -1,13 +1,64 @@
 package Laboratorio_3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GestorObras {
     ArrayList<Obra> listaObras = new ArrayList<>();
 
     public  void createObra(){
+        Obra obraCriada;
         Scanner in = new Scanner(System.in);
+        int choice = 0;
+        System.out.println("Escolha o tipo de obra \n 1.Pintura \n 2.Escultura");
+
+        try {
+            choice = in.nextInt();
+        }catch (Exception e){
+            System.out.println("Invalid input");
+        }
+
+        System.out.println("Introduza autor: ");
+        String autor = in.nextLine();
+
+        System.out.println("Introduza título: ");
+        String titulo = in.nextLine();
+        int ano = 0;
+        try {
+            System.out.println("Introduza o ano: ");
+            ano = in.nextInt();
+        }catch (Exception e ){
+            System.out.println("Invalid input");
+        }
+        if(choice == 1 ){
+
+            String tecnica = "";
+
+            System.out.println(String.format("Escolha a tecnica\n 1 ",TecnicaPintura.OLEO,"\n 2",TecnicaPintura.ACRILICO,"\n 3",TecnicaPintura.AQUARELA));
+
+            int choiceTecnica = in.nextInt();
+
+            if(choiceTecnica ==1){
+                obraCriada = new PinturaOleo(titulo,autor,ano,TecnicaPintura.OLEO);
+                return;
+            }
+            if(choiceTecnica == 2){
+
+                obraCriada = new PinturaOleo(titulo,autor,ano,TecnicaPintura.ACRILICO);
+                return;
+            }
+            if(choiceTecnica == 3 ){
+
+                obraCriada = new PinturaOleo(titulo,autor,ano,TecnicaPintura.AQUARELA);
+                return;
+            }else{
+                System.out.println("Escolha de tecnica invalida");
+            }
+
+
+        }
 
     }
     private void addObra(Obra obra){
--- src/main/java/Laboratorio_3/PinturaOleo.java
+++ src/main/java/Laboratorio_3/PinturaOleo.java
@@ -2,5 +2,8 @@ package Laboratorio_3;
 
 public class PinturaOleo extends Pintura{
 
-  
+
+    public PinturaOleo(String titulo, String autor, int ano, TecnicaPintura tecnicaPintura) {
+        super();
+    }
 }
